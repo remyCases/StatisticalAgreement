Fixing estimation of ccc with ustats method, variance is still broken. Add mc simulation in accords of publication to verify results

main.py
@@ -3,13 +3,25 @@
 # This file is part of adventOfCode project from https://github.com/remyCases/StatiscalAgreement.
 
 import numpy as np
+import argparse
 import src.StatisticalAgreement as sa
 
 if __name__ == "__main__":
-    X = np.array([10, 11, 12, 10, 13, 15, 100, 102, 101, 100, 20, 25, 22, 20, 100, 105, 103, 105])
-    Y = np.array([11, 11, 11, 12, 16, 16, 100, 103, 99, 99, 22, 21, 21, 20, 99, 101, 104, 107])
-    agr = sa.Agreement(X, Y, 
-                       delta_criterion_for_cp=5, 
-                       pi_criterion_for_tdi=0.9)
-    
-    sa.ccc_simulation(n_mc=1000, n_data=10)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--example", "-e", required=False, action='store_true')
+    parser.add_argument("--simulation","-s", required=False, action='store_true')
+
+    args = parser.parse_args()
+
+    if args.simulation:
+        sa.ccc_simulation()
+
+    if args.example:
+        X = np.array([10, 11, 12, 10, 13, 15, 100, 102, 101, 100, 20, 25, 22, 20, 100, 105, 103, 105])
+        Y = np.array([11, 11, 11, 12, 16, 16, 100, 103, 99, 99, 22, 21, 21, 20, 99, 101, 104, 107])
+        agr = sa.Agreement(X, Y, 
+                        delta_criterion_for_cp=5, 
+                        pi_criterion_for_tdi=0.9)
+        agr.ccc_approximation().ccc_ustat()
+        print(agr._ccc)
+        print(agr._ccc_ustat)

src/StatisticalAgreement/agreement.py
@@ -94,42 +94,42 @@ class Agreement:
         # Robust estimators of the concordance correlation coefficient. 
         # J Biopharm Stat. 2001;11(3):83-105. doi: 10.1081/BIP-100107651. PMID: 11725932.
 
+        n = self._n
         sx = np.sum(self._x)
         sy = np.sum(self._y)
         ssx = np.sum(self._x**2)
         ssy = np.sum(self._y**2)
 
         xy = self._x * self._y
-        dss = self._x**2 - ssx / self._n + self._y**2 - ssy / self._n
-        ds_sq = self._x * sy + self._y * sx - sx * sy / self._n
         sxy = np.sum(xy)
 
-        u1 = -4 / (self._n - 1) * sxy
-        u2 = 2 / (self._n - 1) * (ssx + ssy)
-        u3 = -4 / (self._n * (self._n - 1)) * (sx * sy)
+        u1 = -4/n * sxy
+        u2 = 2/n * (ssx + ssy)
+        u3 = -4/(n*(n-1)) * (sx * sy - sxy)
 
-        h = (self._n - 1) * (u3 - u1)
-        g = u1 + self._n * u2+ (self._n - 1)*u3
+        h = (n-1) * (u3 - u1)
+        g = u1 + n*u2 + (n-1)*u3
 
-        v_u1 = 64 / (self._n-1)**2 * np.sum(xy**2)
-        v_u2 = 4 / ((self._n-1)**2 * self._n**2) * np.sum(dss**2)
-        v_u3 = 64 / ((self._n-1)**2 * self._n**2) * np.sum(ds_sq**2)
+        psi1 = (n-2)*xy + sxy/n
+        psi2 = (n-2) * (self._x**2 - ssx / n + self._y**2 - ssy / n)
+        psi3 = (self._x * sy + self._y * sx - sx * sy / n + 2*xy + sxy/n)
 
-        cov_u1_u2 = -16 / (self._n-1)**2 * np.sum(xy * dss)
-        cov_u1_u3 = 64 / ((self._n-1)**2 * self._n) * np.sum(xy * ds_sq)
-        cov_u2_u3 = -16 / ((self._n-1)**2 * self._n) * np.sum(dss * ds_sq)
-
-        s_sq_h = (self._n-1)**2 * (v_u3 + v_u1 - 2 * cov_u1_u3)
-        s_sq_g = (self._n-1)**2 * v_u3 + v_u1 + self._n**2 * v_u2 + 2*(self._n-1) * cov_u1_u3 \
-            + 2 * self._n**2 * cov_u2_u3
-        s_hg = -(self._n-1)*(self._n-2)*cov_u1_u3 + self._n*(self._n-1)*cov_u2_u3 + (self._n-1)**2*v_u3 \
-            - (self._n-1)*v_u1 - self._n*(self._n-1)*cov_u1_u2
+        v_u1 = 64*np.sum(psi1**2)/(n**2*(n-1)**2)
+        v_u2 = 4*np.sum(psi2**2)/(n**2*(n-1)**2)
+        v_u3 = 64*np.sum(psi3**2)/(n**2*(n-1)**2)
+        cov_u1_u2 = -16*np.sum(psi1*psi2)/(n**2*(n-1)**2)
+        cov_u1_u3 = 64*np.sum(psi1*psi3)/(n**2*(n-1)**2)   
+        cov_u2_u3 = -16*np.sum(psi2*psi3)/(n**2*(n-1)**2)
+        
+        s_sq_h = (n-1)**2 * (v_u3 + v_u1 - 2 * cov_u1_u3)
+        s_sq_g = (n-1)**2*v_u3 + v_u1 + n**2*v_u2 + 2*(n-1)*cov_u1_u3 + 2*n*cov_u2_u3
+        s_hg = -(n-1)*(n-2)*cov_u1_u3 + n*(n-1)*cov_u2_u3 + (n-1)**2*v_u3 - (n-1)*v_u1 - n*(n-1)*cov_u1_u2
         
         ccc_hat = h/g
-        var_ccc_hat = ccc_hat**2 * (s_sq_h / h**2 - 2 * s_hg / (h * g) + s_sq_g / g**2)
+        var_ccc_hat = ccc_hat**2 * (s_sq_h / h**2 - 2*s_hg / (h*g) + s_sq_g / g**2)
         var_z_hat = var_ccc_hat / (1 - ccc_hat**2)**2
-
-        ccc_range = TransformEstimator(ccc_hat, var_ccc_hat, TransformFunc.Z)
+        
+        ccc_range = TransformEstimator(ccc_hat, var_z_hat, TransformFunc.Z)
         self._ccc_ustat = Estimator(
             name="ccc_ustat",
             estimator=ccc_hat,

src/StatisticalAgreement/ccc_simulation.py
@@ -3,29 +3,75 @@
 # This file is part of adventOfCode project from https://github.com/remyCases/StatiscalAgreement.
 
 import numpy as np
-from scipy.stats import multivariate_normal
+import numpy.typing as npt
+from typing import NamedTuple
+import pandas as pd
+from itertools import product
+from scipy.stats import multivariate_normal, shapiro
 import src.StatisticalAgreement as sa
 from .monte_carlo import MonteCarlo
-import matplotlib.pyplot as plt
 
-def ccc_simulation(n_mc: int, n_data: int):
-    mean = np.array([0, 0])
-    cov = np.array([[1, 0.95], [0.95, 1]])
-    array_ccc_ustat = np.empty(n_mc)
-    array_z_ustat = np.empty(n_mc)
+class Models(NamedTuple):
+    mean: npt.ArrayLike
+    cov: npt.ArrayLike
+
+# Based on 
+# King TS, Chinchilli VM. 
+# Robust estimators of the concordance correlation coefficient. 
+# J Biopharm Stat. 2001;11(3):83-105. doi: 10.1081/BIP-100107651. PMID: 11725932.
+# pages 90-94 
+MODELS = {
+    '1': Models(mean=np.array([0, 0]), 
+                cov=np.array([[1, 0.95], [0.95, 1]])),
+    '2': Models(mean=np.array([-np.sqrt(0.1)/2, np.sqrt(0.1)/2]), 
+                cov=np.array([[1.1**2, 0.95*1.1*0.9], [0.95*1.1*0.9, 0.9**2]])),
+    '3': Models(mean=np.array([-np.sqrt(0.25)/2, np.sqrt(0.25)/2]), 
+                cov=np.array([[(4/3)**2, 0.5*4/3*2/3], [0.5*4/3*2/3, (2/3)**2]]))}
+
+def ccc_simulation():
+    data_possibilities = [10, 20, 40, 80]
+    models_possibilities = ['1', '2', '3']
+    tupleIndex = tuple(product(data_possibilities, models_possibilities, ['ccc', 'z']))
+    multiIndex = pd.MultiIndex.from_tuples(tupleIndex, names=('n', 'case', 'estimator'))
+    
+    tupleCol = tuple(product(models_possibilities, data_possibilities))
+    multiCol = pd.MultiIndex.from_tuples(tupleCol, names=('case', 'n'))
+
+    normalityDf = pd.DataFrame(index=multiIndex, columns=['pvalue'])
+    estimateDf = pd.DataFrame(index=['ccc (mc estimate of ccc)', 
+                                     'sigma_sq_z (mc estimate of z-variance)', 
+                                     'var_z (variance of mc estimation of z)'], columns=multiCol)
+
+    for m in models_possibilities:
+        for d in data_possibilities:
+            ccc_simulation_from_model_and_ndata(nIteration=1000, nData=d, model=m, 
+                                                normalityDf=normalityDf, estimateDf=estimateDf)
+    print(normalityDf, '\n')
+    print(estimateDf, '\n')
+
+def ccc_simulation_from_model_and_ndata(nIteration: int, nData: int, model: str, normalityDf, estimateDf):
+    mean, cov = MODELS[model]
+    array_ccc_ustat = np.empty(nIteration)
+    array_z_ustat = np.empty(nIteration)
+    array_z_ustat_var = np.empty(nIteration)
 
     mc_ustat = MonteCarlo()
 
-    for i in range(n_mc):
-        multidim = multivariate_normal.rvs(mean=mean, cov=cov, size=n_data)
+    for i in range(nIteration):
+        multidim = multivariate_normal.rvs(mean=mean, cov=cov, size=nData)
         agrtest = sa.Agreement(multidim[:, 0], multidim[:, 1])
         agrtest.ccc_ustat()
         array_ccc_ustat[i] = agrtest._ccc_ustat.estimator
         array_z_ustat[i] = agrtest._z_ustat.estimator
+        array_z_ustat_var[i] = agrtest._z_ustat.variance
 
-    plt.hist(array_ccc_ustat)
-    plt.show()
-    plt.hist(array_z_ustat)
-    plt.show()
-    print(f"{n_data} ccc : {mc_ustat.compute(array_ccc_ustat)}")
-    print(f"{n_data} z : {mc_ustat.compute(array_z_ustat)}")
+    mc_ccc_res = mc_ustat.compute(array_ccc_ustat)
+    mc_z_res = mc_ustat.compute(array_z_ustat)
+    mc_z_var_res = mc_ustat.compute(array_z_ustat_var)
+    
+    normalityDf.loc[(nData, model, 'ccc'), 'pvalue'] = shapiro(array_ccc_ustat).pvalue
+    normalityDf.loc[(nData, model, 'z'), 'pvalue'] = shapiro(array_z_ustat).pvalue
+
+    estimateDf.loc['ccc (mc estimate of ccc)', (model, nData)] = mc_ccc_res.mean
+    estimateDf.loc['sigma_sq_z (mc estimate of z-variance)', (model, nData)] = mc_z_var_res.mean
+    estimateDf.loc['var_z (variance of mc estimation of z)', (model, nData)] = mc_z_res.var

src/StatisticalAgreement/monte_carlo.py
@@ -3,6 +3,13 @@
 # This file is part of adventOfCode project from https://github.com/remyCases/StatiscalAgreement.
 
 import numpy as np
+from typing import NamedTuple
+
+class McCi(NamedTuple):
+    mean: float
+    var: float
+    lower: float
+    upper: float
 
 class MonteCarlo():
     def __init__(self):
@@ -23,4 +30,9 @@ class MonteCarlo():
 
         self._mean = self._sum / self._n
         self._var = self._sum_sq / self._n - self._mean * self._mean
-        return self._mean, self._var
+        return McCi(
+            mean=self._mean,
+            var=self._var,
+            lower=self._mean - 1.96*np.sqrt(self._var/self._n),
+            upper=self._mean + 1.96*np.sqrt(self._var/self._n)
+            )