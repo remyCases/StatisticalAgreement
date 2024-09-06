5b4b69d7dcf194ab8edb17d17c1a63dd68049c43
remyCases <remy.cases@gmail.com>
2024-09-06 11:47:11 +0200

    [Minor] adds docstring for ulp equality

--- StatisticalAgreement/core/mathutils.py
+++ StatisticalAgreement/core/mathutils.py
@@ -5,26 +5,43 @@
 import numpy as np
 
 def almost_equal_float(first: float, second: float, max_ulps=4) -> bool:
+    '''
+    Asserts if two floats are almost equal.
+
+    Parameters
+    ----------
+    first : float
+    second : float
+    max_ulps : int, default: 4
+        Maximum difference of ulp to assert equality.
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    >>> almost_equal_float(1.0, 1.01)
+    False
+    '''
     # using difference of ulp as a more reliable way to test equality in float/double
     # same method as used in GoogleTest C++ library
     # see https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
 
     # catching number with difference of sign and inf/nan
-    if (first < 0 != second < 0):
+    if first < 0 != second < 0:
 
         # 0 == -0 can't be catched by integer representation
-        if (first == second):
+        if first == second:
             return True
         return False
-    
-    else:
 
-        first_int = np.array(first).view('int64')
-        second_int = np.array(second).view('int64')
+    first_int = np.array(first).view('int64')
+    second_int = np.array(second).view('int64')
 
-        ulps_diff = abs(first_int - second_int)
+    ulps_diff = abs(first_int - second_int)
 
-        if ulps_diff < max_ulps:
-            return True
-   
+    if ulps_diff < max_ulps:
+        return True
+
     return False
